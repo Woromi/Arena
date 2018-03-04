9d450c210745fd5fdda067e04c4bc3e34987e3e0
Woromi <nad1997@seznam.cz>
2018-03-04 15:59:12 +0100

    mazání komentů

--- Arena/Source.cpp
+++ Arena/Source.cpp
@@ -14,31 +14,20 @@ int main(int argc, char * * argv) {
 	// Vytvareni tymu bez cteni vstupu (jen pro testovani souboje)
 	Arena arena;
 	{	
-		//Mage Woromi{ "Woromi" };
-		//Mage Modrozub{ "Modrozub" };
 		Mage Plosnak{ "Plosnak" };
 		Mage Plosnak2{ "Plosnak2" };
 		Mage Dummy1{ "Dummy1" };
 		Mage Dummy2{ "Dummy2" };
-		//Woromi.learn(arena.out, knihovna.get_spell(arena.out, "Ice lance"));
-		//Modrozub.learn(arena.out, knihovna.get_spell(arena.out, "Fireball"));
 		Plosnak.learn(arena.out, knihovna.get_spell(arena.out, "Flamestrike"));
 		Plosnak2.learn(arena.out, knihovna.get_spell(arena.out, "Flamestrike"));
-		//Dummy1.learn(arena.out, knihovna.get_spell(arena.out, "Ice lance"));
 		Dummy2.learn(arena.out, knihovna.get_spell(arena.out, "Ice lance"));
 
-		//Woromi.buy_weapon(arena.out, shop.get_weapon(arena.out, "Wooden staff"));
-		//Woromi.buy_robe(arena.out, shop.get_robe(arena.out, "Heavy armored robe"));
-		//Modrozub.buy_robe(arena.out, shop.get_robe(arena.out, "Robe"));
-		//Modrozub.buy_weapon(arena.out, shop.get_weapon(arena.out, "Wooden staff of flames"));
 		Plosnak.buy_weapon(arena.out, shop.get_weapon(arena.out, "Wooden staff of flames"));
 		Plosnak.buy_robe(arena.out, shop.get_robe(arena.out, "Heavy armored robe"));
 		Dummy1.buy_robe(arena.out, shop.get_robe(arena.out, "Robe of fire resistance"));
 
-		//arena.team1_add(std::move(Woromi));
 		arena.team1_add(std::move(Plosnak));
 		arena.team1_add(std::move(Plosnak2));
-		//arena.team2_add(Modrozub);
 		arena.team2_add(std::move(Dummy1));
 		arena.team2_add(std::move(Dummy2));
 	}//*/
